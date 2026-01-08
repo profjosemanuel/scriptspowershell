f5daa3677447259006ad8eed38c8e9738372404f
José Manuel Alvarez <97788933+profjosemanuel@users.noreply.github.com>
2026-01-08 21:40:32 +0000

    prueba

--- leercsv.ps1
+++ leercsv.ps1
@@ -1,11 +1,12 @@
 # Definir la ruta del archivo
-$ruta = "C:\ruta\tu_archivo.csv"
+$ruta = ".\Test.csv"
 
 # Verificar si el archivo existe antes de intentar leerlo
 if (Test-Path $ruta) {
     Write-Host "--- Contenido del Archivo CSV ---" -ForegroundColor Cyan
     $datos = Import-Csv -Path $ruta -Delimiter ","
-    $datos | Format-Table -AutoSize
+#    $datos | Format-Table -AutoSize
+    $datos[4]
 } else {
     Write-Host "Error: No se encontró el archivo en $ruta" -ForegroundColor Red
 }
